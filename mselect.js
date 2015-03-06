7d0dd29eedee8750b67fa809b794ec3fa043f455
arthur.zielinski <arthur.zielinski@posterxxl.com>
2015-03-06 18:05:50 +0100

    prototype

---
+++ mselect.js
@@ -0,0 +1,135 @@
+/* 
+ * author: Arthur Zielinski 
+ * version: 1.01 
+ */
+
+'use strict';
+var Mselect = (function () {
+	
+	function Mselect(id, array) {
+
+		var el  	 = document.getElementById(id);	 
+		var source   = el.querySelector('.source');
+		var dest 	 = el.querySelector('.dest');
+		var selector = function(value) { return "[data-value='"+value+"']" };
+		var data 	 = [];
+		var _self 	 = this;
+		var _onChange;	
+
+
+		var select = function(e, triggerEvent) {	 
+			var selected = e.cloneNode(true);
+				
+			dest.appendChild(selected);			  				
+			data.push(e.getAttribute('data-value'));
+			e.style.display = 'none';	  						
+
+			if( triggerEvent !== false ) {
+				triggerOnChange();
+			}
+		};
+
+
+		var unselect = function(e) {	  	
+			var value = e.getAttribute('data-value');
+			var i = data.indexOf(value);		
+			var s = selector(value);
+			data.splice(i, 1);
+
+			dest.querySelector(s).remove();	  				
+			source.querySelector(s).style.display = 'list-item';	
+			
+			triggerOnChange();
+		};
+
+
+		var build = function(data) {					
+			var html = '';
+
+			for( var i=0; i<data.length; i++ ) {
+				html += createElement(data[i].name, data[i].value);
+			}						 
+			
+			source.innerHTML = html;
+			source.addEventListener('click', function(e) {
+				if( e.target != source ) {							
+					select(e.target); 
+				}
+			});
+
+			dest.addEventListener('click', function(e) {
+				if( e.target != dest ) {							
+					unselect(e.target); 
+				}
+			});
+		};
+
+
+		var createElement = function(name, value) {
+			if( name === undefined ) {
+				name = value;
+			}
+			return '<li data-value="'+value+'">'+name+'</li>';
+		};
+
+
+		_self.selectItems = function(data) {			
+			for(var i = 0; i < data.length; i++) {
+				var s = selector(data[i]);
+				var e = source.querySelector(s);
+				select(e, false);
+			}
+
+			triggerOnChange();
+		};
+
+
+		_self.selectAllItems = function() {
+
+			// TODO
+			console.log(source.children.length);
+
+			for(var i = 0; i < source.length; i++) {
+				console.log(source[i]);
+				return;
+				select(e, false);
+			}
+			triggerOnChange();
+		};
+
+
+		_self.reset = function() {
+			// faster
+			[].map.call(document.querySelectorAll("[style='display: none;']"),function(item) { 
+				item.style.display = 'list-item'; 
+			});
+			data = [];
+			dest.innerHTML = '';
+			triggerOnChange();
+		}
+
+		// events
+		_self.onChange = function(callback) {			
+			_onChange = callback;						
+		};
+
+		var triggerOnChange = function() {
+			if( _onChange && typeof _onChange == "function" ) {
+				_onChange(data);  				
+			}
+		}
+
+		// getter
+		_self.getData = function() {
+			return data;
+		}
+
+
+		build(array);
+	}
+
+	return Mselect;
+})();
+
+	
+
